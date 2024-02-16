54485b1851f717b7561860df64be40bfbb207267
madhulikaa223344 <madhulikaa223344@gmail.com>
2024-02-16 13:59:18 +0530

    added auth functionality

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router,Route, Routes } from "react-router-dom"
+import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
+import SignIn from "./pages/SignIn";
+
+const App=()=>{
+  return(
+    <Router>
+    <Routes>
+    <Route path="/" element={<Layout><p>Home Page</p></Layout>}/>
+    <Route path="/search" element={<Layout>{/* <Search /> */}<p>Search Page</p></Layout>}/>
+    <Route path="/register" element={<Layout><Register /></Layout>}/> 
+    <Route path="/sign-in" element={<Layout><SignIn /></Layout>}/>
+    </Routes>
+    </Router>
+  )
+}
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,24 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [ "./index.html"," ./src/**/*.{html,js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//     container:{
+//       padding:"10rem",
+//     }
+//   },
+//   plugins: [],
+// }
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    container: {
+      padding: {
+        md: "10rem",
+      },
+    },
+  },
+  plugins: [],
+};
